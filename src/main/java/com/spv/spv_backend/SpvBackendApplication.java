1365c14c9a6aa87c1e203663389309a0fe234988
rodrivmac1 <166084548+rodrivmac1@users.noreply.github.com>
2026-08-24 22:52:42 -0600

    Initial commit

---
+++ src/main/java/com/spv/spv_backend/SpvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spv.spv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spv/spv_backend/SpvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spv.spv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
